01f81f3ad84fa81c3ee9554a2c104c4bcf32e561
Elex <maharjanelex@gmail.com>
2020-03-07 06:46:50 +0545

    ok

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const http = require('http')
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/my-catering-app/')));
+app.use(express.static(path.join(__dirname, '/dist/my-catering-app')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/my-catering-app/index.html'));
