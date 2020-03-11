f4ad8721f7497f2e01f83253dbf4adfce946c097
Elex <maharjanelex@gmail.com>
2020-03-12 05:02:14 +0545

    ok

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+import * as cors from "cors";
+
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const request = require('request');
 
 const app = express();
+
+app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static(path.join(__dirname, '/dist/my-catering-app')));
 
@@ -14,4 +25,4 @@ const port = process.env.PORT || 4200;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+server.listen(port, () => console.log('running on port 4200'));
