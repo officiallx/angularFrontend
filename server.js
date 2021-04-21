42823bc58cb79d75312224729cd2ddf46498b936
Elex Maharjan <chimprockofficial@gmail.com>
2021-04-22 00:52:35 +0545

    bhayenanananna

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(expresss.static('/src/index.html'));
+app.use(expresss.static('src/index.html'));
 //app.use(express.static(path.join(__dirname, './dist/my-catering-app')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + 'src/index.html'));
 });
 
-/*app.post('*', (req, res) => {
+app.post('*', (req, res) => {
   res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});*/
+});
 
 const port = process.env.PORT || 4200;
 app.set('port', port);
