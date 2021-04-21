d5baa2a582ff083d7bf00f9414b8c36ed770bb9e
Elex <chimprockofficial@gmail.com>
2021-04-21 23:57:39 +0545

    dooone

--- src/app/server.js
+++ src/app/server.js
@@ -1,20 +1,20 @@
-const expresss = require('express');
+const express = require('express');
 const http = require('http');
 const path = require('path');
 const request = require('request');
 
 const cor = require('cors');
 
-const app = expresss();
+const app = express();
 
 app.use(cor());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(expresss.static('./../dist/my-catering-app'));
+app.use(express.static('./../dist/my-catering-app'));
 //app.use(express.static(path.join(__dirname, './dist/my-catering-app')));
 
 app.get('*', (req, res) => {
